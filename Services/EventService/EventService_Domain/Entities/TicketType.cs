91d9d31f5c0b68c9e426de09998c5b1f74f4b159
quanghoaluu1 <quanghoaluu1@gmail.com>
2026-02-11 18:34:54 +0700

    Remove SaleEndUnit and SaleEndWhen properties from TicketType and related DTOs; update validation logic in TicketTypeService

--- Services/EventService/EventService_Domain/Entities/TicketType.cs
+++ Services/EventService/EventService_Domain/Entities/TicketType.cs
@@ -15,8 +15,6 @@ public class TicketType
     public int QuantitySold { get; set; }
     public DateTime SaleStartDate { get; set; }
     public DateTime SaleEndTime { get; set; } //Thời gian kết thúc trước khi đóng vé
-    public SaleEndUnit SaleEndUnit { get; set; } //Đơn vị kết thúc đóng vé 
-    public SaleEndWhen SaleEndWhen { get; set; } //Kết thúc khi nào //Vd: 1 ngày (day) trước khi event start (1)
     public int MinPerOrder { get; set; }
     public int MaxPerOrder { get; set; }
     public bool IsRequireHolderInfo { get; set; }
