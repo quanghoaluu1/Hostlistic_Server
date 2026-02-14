0fa63f4ffc19093d20f21dd63b1fb0a8616f1429
Mhin <96234654+Mhin28@users.noreply.github.com>
2026-02-14 20:47:36 +0700

    update auth controller

--- Services/IdentityService/IdentityService_Api/Controllers/AuthController.cs
+++ Services/IdentityService/IdentityService_Api/Controllers/AuthController.cs
@@ -46,17 +46,17 @@ public class AuthController(IAuthService authService) : ControllerBase
     }
 
     [HttpPost("forgot-password")]
-    public async Task<IActionResult> SendResetPasswordEmail(string email)
+    public async Task<IActionResult> SendResetPasswordEmail([FromBody] ForgotPasswordRequest request)
     {
-        var result = await authService.RequestPasswordResetAsync(email);
+        var result = await authService.RequestPasswordResetAsync(request.Email);
         if (!result.IsSuccess) return BadRequest(result);
         return Ok(result);
     }
 
     [HttpPost("reset-password")]
-    public async Task<IActionResult> ResetPasswordAsync(string email, string otp, string newPassword)
+    public async Task<IActionResult> ResetPasswordAsync([FromBody] ResetPasswordRequest request)
     {
-        var result = await authService.ResetPasswordAsync(email, otp, newPassword);
+        var result = await authService.ResetPasswordAsync(request.Email, request.Otp, request.NewPassword);
         if (!result.IsSuccess) return BadRequest(result);
         return Ok(result);
     }
