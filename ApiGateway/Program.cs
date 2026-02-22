60cf74628876e0c1fef578e401e47251dd31a356
This is Hòa <quanghoaluu1@gmail.com>
2026-02-22 20:25:43 +0700

    Add CORS policy for frontend connection and configure YARP reverse proxy in ApiGateway

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("NextApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors("NextApp");
+
+app.MapReverseProxy();
 
 app.Run();
