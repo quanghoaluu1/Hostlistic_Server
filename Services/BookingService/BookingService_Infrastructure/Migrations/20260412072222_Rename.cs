18a9a73eb2a8466df12d0303f26490514d7bfcbb
This is Hòa <quanghoaluu1@gmail.com>
2026-04-12 14:26:44 +0700

    Rename `EventName` to `EventTitle` in `EventSettlement` and DTOs, update migration and database snapshot.

---
+++ Services/BookingService/BookingService_Infrastructure/Migrations/20260412072222_Rename.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingService_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Rename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EventName",
+                table: "EventSettlements",
+                newName: "EventTitle");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EventTitle",
+                table: "EventSettlements",
+                newName: "EventName");
+        }
+    }
+}
